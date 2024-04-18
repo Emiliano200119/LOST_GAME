d78beae8f9679ddb2ec7c45b10f0ffcfb313ca6c
Emiliano200119 <166468072+Emiliano200119@users.noreply.github.com>
2024-04-18 14:13:23 -0600

    Objetos inutiles y audio fantasma
    Se eliminaron carpetas vacias
    
    Se creo la reproduccion del audio cuando se spawnea un fantasma.

--- Lost Game/Assets/Scripts/ActivarFantasma.cs
+++ Lost Game/Assets/Scripts/ActivarFantasma.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class ActivarFantasma : MonoBehaviour
 {
-    public Spawn spawner; // Referencia al script de spawner
+    public Spawn spawner;
+    public AudioClip sonidoEntradaFantasma; 
+    private bool zonaFantasmaActivada = false; 
 
     private void OnTriggerEnter(Collider other)
     {
-        // Verificar si el jugador entró en la zona específica
-        if (other.CompareTag("Player"))
+        
+        if (other.CompareTag("Player") && !zonaFantasmaActivada)
         {
-            // Llamar al método para spawnear el objeto fantasma
+            if (sonidoEntradaFantasma != null)
+            {
+                AudioSource.PlayClipAtPoint(sonidoEntradaFantasma, transform.position);
+            }
+
             spawner.SpawnFantasma();
+
+            zonaFantasmaActivada = true;
+
+            GetComponent<Collider>().enabled = false;
         }
     }
 }
