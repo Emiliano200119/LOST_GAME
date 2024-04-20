cfaacf7eec6af3eac35e509299d99fdae67caa52
Emiliano200119 <166468072+Emiliano200119@users.noreply.github.com>
2024-04-19 21:22:04 -0600

    Interfaz Final
    Se configuro y creo la interfaz final del juego.

--- Lost Game/Assets/Scripts/Next_Level.cs
+++ Lost Game/Assets/Scripts/Next_Level.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Next_Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Verificar si se ha presionado una tecla específica (por ejemplo, la tecla "Escape")
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Restaurar la posición del jugador antes de cargar la escena "Lost_Escene"
+            SceneManager.LoadScene("Final_Interfaz");
+        }
     }
 }
